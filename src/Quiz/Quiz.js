2f6ff607e5859324bc725d6460944ecd3dfa720f
the critical thinker <boycaro19990@gmail.com>
2023-03-25 20:28:55 +0100

    iii

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -41,13 +41,14 @@ const Quiz = () => {
   return (
     <div className="flex w-full justify-center  bg-yellow-300 ">
       <div className="flex flex-col w-3/4 justify-center items-center">
-        <img className="w-1/6 mt-3 " src={know} alt="Happy" />
-        <button className="flex flex-col md:flex-roww-full w-full md:w-5/6 mt-3 mb-3 bg-cyan-400 shadow-slate-700 shadow-md text-5xl hover:bg-slate-100 h-32 md:h-20 rounded-2xl ">
+        <img className="w-2/6 mt-3 " src={know} alt="Happy" />
+        <button className="flex flex-col md:flex-row w-full justify-center items-center md:w-5/6 mt-3 mb-3  bg-cyan-400 shadow-slate-700 shadow-md text-5xl hover:bg-slate-100 h-32 md:h-20 rounded-2xl ">
           {Quiz.name}
         </button>
-        <div className=" flex w-2/6 p-3 justify-center items-center rounded-full bg-slate-400">
+        <div className=" flex w-2/3 md:flex-row flex-col md:w-1/6 p-3 justify-center items-center rounded-full bg-slate-400">
+          {/* <div className="flex flex-col md:flex-row w-full "></div> */}
           <img
-            className="w-full h-full "
+            className="w-full"
             src={images[photo]}
             alt="man"
             onClick={() => {
@@ -55,17 +56,21 @@ const Quiz = () => {
             }}
           />
         </div>
+        <h1 className="text-center text-xl">
+          Click sur la photo pour changer d'avatar !
+        </h1>
+
         <input
           type="text"
           placeholder=" Your Name Here"
-          className="h-14 mt-5 mb-5 w-3/6 rounded-2xl text-center"
+          className="h-14 mt-2 mb-5 w-full  md:w-3/6 rounded-2xl text-center"
           onChange={(e) => {
             SetName(e.target.value);
           }}
         />
         <button
           onClick={() => CreateAndPlay()}
-          className="w-3/6 mt-3 mb-3 bg-green-400 shadow-slate-700 shadow-md text-5xl hover:bg-slate-100 h-14 rounded-2xl "
+          className=" w-full md:w-3/6 mt-3 mb-3 bg-green-400 shadow-slate-700 shadow-md text-5xl hover:bg-slate-100 h-14 rounded-2xl "
         >
           Start
         </button>
