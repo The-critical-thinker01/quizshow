64d93108b4b6af62795ecb16efe9d7cca67b13ac
the critical thinker <boycaro19990@gmail.com>
2023-03-25 17:35:14 +0100

    e

--- src/Pages/FinishPage/FinishPage.js
+++ src/Pages/FinishPage/FinishPage.js
@@ -5,64 +5,64 @@ import { GetPlayer, RetryPlay } from "../../API/Services/quizService";
 import QuestionAndAnswer from "./QuestionAndAnswer";
 import man from "../../asset/images/man.png";
 const FinishPage = () => {
+  const { id, playerid } = useParams();
 
-    const { id, playerid } = useParams();
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [Player, SetPlayer] = useState({ answers: [] });
 
-    const [Player, SetPlayer] = useState({ answers: [] });
+  const [points, SetPoints] = useState(0);
+  const [totals, SetTotal] = useState(0);
 
-    const [points, SetPoints] = useState(0);
-    const [totals, SetTotal] = useState(0);
+  useEffect(() => {
+    GetPlayer(playerid)
+      .then((res) => {
+        SetPlayer(res.data);
+        let numberofGood = 0;
+        res.data.answers.forEach((item) => {
+          if (item.question.answer === item.answer) numberofGood++;
+        });
+        SetTotal(res.data.answers.length);
+        SetPoints(numberofGood);
+        console.log();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [playerid]);
 
-    useEffect(() => {
-        GetPlayer(playerid).then((res) => {
-            SetPlayer(res.data);
-            let numberofGood = 0;
-            res.data.answers.forEach(item => {
-                if (item.question.answer === item.answer)
-                    numberofGood++;
-            });
-            SetTotal(res.data.answers.length)
-            SetPoints(numberofGood);
-            console.log();
-        }).catch((err) => {
-            console.log(err)
-        })
-    }, [playerid])
-
-    const Retry = async () => {
-        try {
-            const player = await RetryPlay(playerid)
-            console.log(player)
-            navigate(`/game/${id}/${playerid}`)
-
-        } catch (err) {
-            console.log(err)
-        }
+  const Retry = async () => {
+    try {
+      const player = await RetryPlay(playerid);
+      console.log(player);
+      navigate(`/game/${id}/${playerid}`);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return (<div className="flex w-full justify-center  bg-yellow-300 ">
-        <div className="flex flex-col w-3/4 justify-center items-center">
-
-            <div className=" flex w-1/6 p-3 justify-center items-center rounded-full bg-slate-400">
-                <img
-                    className="w-full h-full "
-                    src={man}
-                    alt="man"
-                />
-            </div>
-            <button className="w-5/6 mt-3 mb-3 bg-cyan-400 shadow-slate-700 shadow-md text-5xl hover:bg-slate-100 h-20 rounded-2xl ">
-                {Player.name} <span className="mx-4 text-blue-600"> {points}  / {totals}  pts </span>
-            </button>
-            <QuestionAndAnswer answers={Player.answers} />
-            <button onClick={() => Retry()} className="w-3/6 mt-3 mb-3 bg-green-400 shadow-slate-700 shadow-md text-5xl hover:bg-slate-100 h-14 rounded-2xl ">
-                Retry
-            </button>
+  return (
+    <div className="flex w-full justify-center  bg-yellow-300 ">
+      <div className="flex flex-col w-3/4 justify-center items-center">
+        <div className=" flex w-1/6 p-3 justify-center items-center rounded-full bg-slate-400">
+          <img className="w-full h-full " src={man} alt="man" />
         </div>
+        <button className="flex flex-col justify-center items-center md:flex-row w-full  md:w-5/6 mt-3 mb-3 bg-cyan-400 shadow-slate-700 shadow-md  text-2xl md:text-5xl hover:bg-slate-100 h-20 rounded-2xl ">
+          {Player.name}{" "}
+          <span className="mx-4 text-blue-600">
+            {" "}
+            {points} / {totals} pts{" "}
+          </span>
+        </button>
+        <QuestionAndAnswer answers={Player.answers} />
+        <button
+          onClick={() => Retry()}
+          className="w-3/6 mt-3 mb-3 bg-green-400 shadow-slate-700 shadow-md text-5xl hover:bg-slate-100 h-14 rounded-2xl "
+        >
+          Retry
+        </button>
+      </div>
     </div>
-    );
-
-
+  );
 };
 export default FinishPage;
--- src/Pages/Share/Share.js
+++ src/Pages/Share/Share.js
@@ -22,17 +22,17 @@ const Share = () => {
 
   return (
     <div className="flex w-full justify-center p-4 bg-yellow-300 min-h-screen">
-      <div className="flex flex-col w-3/4 p-4 bg-white rounded-lg">
+      <div className="flex flex-col w-3/4 p-1 bg-white rounded-lg">
         <div className="w-full flex flex-row flex-wrap justify-center items-center bg-white rounded-xl">
-          <div className="w-full flex flex-col flex-wrap justify-center border-b p-4">
-            <div className="flex justify-between">
-              <h2 className="text-xl font-black">{QuizName}</h2>
-              <div className="flex space-x-2">
+          <div className="w-full flex flex-col flex-wrap justify-center border-b">
+            <div className="flex md:flex-row flex-col w-full md:justify-between">
+              <h2 className="w-full md:text-xl  font-black mb-3">{QuizName}</h2>
+              <div className="grid grid-cols-2 w-full gap-2 h-14 md:flex md:space-x-2 ">
                 <button
                   onClick={() => {
                     navigate(`/questionsboard/${id}`);
                   }}
-                  className="p-3  bg-green-400 shadow-slate-700 shadow-md text-lg hover:bg-slate-100 rounded-2xl "
+                  className="md:p-3 w-full h-full  bg-green-400 shadow-slate-700 shadow-md text-lg hover:bg-slate-100 rounded-2xl "
                 >
                   Questions
                 </button>
@@ -40,7 +40,7 @@ const Share = () => {
                   onClick={() => {
                     navigate(`/players/${id}`);
                   }}
-                  className="p-3  bg-green-400 shadow-slate-700 shadow-md text-lg hover:bg-slate-100 rounded-2xl "
+                  className="md:p-3 w-full h-full bg-green-400 shadow-slate-700 shadow-md text-lg hover:bg-slate-100 rounded-2xl "
                 >
                   Players
                 </button>
@@ -49,26 +49,26 @@ const Share = () => {
           </div>
         </div>
 
-        <div className="mt-10 flex justify-center">
+        <div className="mt-10 flex justify-center items-center">
           <a href={`/quiz/${id}`} target="_blank" rel="noopener noreferrer">
             <button className="p-3  bg-green-400 shadow-slate-700 shadow-md text-lg hover:bg-slate-100 rounded-2xl ">
               Play Quiz
             </button>
           </a>
         </div>
-        <div className="flex p-5 justify-around">
+        <div className="flex md:flex-row flex-col mt-8 md:p-5 justify-around">
           <input
             value={link}
-            className="p-4 border-2 rounded-lg w-1/2 outline-none"
+            className="p-1 md:p-4 border-2 rounded-lg w-full md:w-1/2 outline-none"
           />
           <button
             onClick={() => {
               navigator.clipboard.writeText(link);
               alert("Link copied");
             }}
-            className="p-3 w-1/3 bg-green-400 shadow-slate-700 shadow-md text-lg hover:bg-slate-100 rounded-2xl "
+            className=" p-1 mt-5 md:p-3 w-full md:w-1/3 bg-green-400 shadow-slate-700 shadow-md text-lg hover:bg-slate-100 rounded-2xl "
           >
-            Copy Link
+            Copy quiz Link
           </button>
         </div>
       </div>
